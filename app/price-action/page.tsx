e5fa9d8b80b323881cd18c8e681d90f04834a95e
SwapKam <swapkx@gmail.com>
2024-10-09 23:34:22 +0530

    New background

--- app/price-action/page.tsx
+++ app/price-action/page.tsx
@@ -1,40 +1,49 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-"use client";
-import { useEffect, useState } from "react";
-
 export default function PriceAction() {
-  const colors = [
-    "hsl(273, 100%, 41%)",
-    "hsl(275, 100%, 41%)",
-    "hsl(245, 100%, 41%)",
-    "hsl(289, 100%, 51%)",
-    "hsl(252, 100%, 51%)",
-    "hsl(254, 100%, 68%)",
-  ];
-
-  const [colorIndex, setColorIndex] = useState(0);
-
-  // Function to update the text color based on the array
-  const updateTextColorOverTime = () => {
-    setColorIndex((prevIndex) => (prevIndex + 1) % colors.length); // Cycle through the colors
-  };
-
-  // useEffect for time-based text color update
-  useEffect(() => {
-    const intervalId = setInterval(updateTextColorOverTime, 2000); // Update every 2 seconds
-    return () => clearInterval(intervalId); // Clean up interval on component unmount
-  }, []);
-
   return (
-    <main className="w-full h-screen flex items-center justify-center">
+    <main>
       <div className="h-96 max-w-screen-2xl mx-auto px-4 flex items-center justify-center">
-        <h1
-          className="text-2xl md:text-8xl font-bold transition-colors duration-500 ease-in-out"
-          style={{ color: colors[colorIndex] }}
-        >
-          Comming Soon!
+        <h1>
+          <span className="bg-gradient-to-r from-blue-500 to-[#E114E5] bg-clip-text text-transparent">
+            Comming Soon!
+          </span>
         </h1>
       </div>
+      <section className="relative">
+        <div className="m-auto h-80 bg-gradient-to-tr from-indigo-400 via-teal-900 to-[#C084FC] blur-[118px]"></div>
+
+        <div className="relative mx-auto max-w-screen-xl gap-12 px-4 py-28 text-slate-400 md:px-8">
+          <div className="mx-auto max-w-4xl space-y-5 text-center">
+            <h1 className="text-sm font-medium text-blue-500">
+              Build products for everyone
+            </h1>
+            <h2 className="mx-auto text-4xl font-extrabold text-blue-600 md:text-5xl">
+              Design your projects faster with{" "}
+              <span className="bg-gradient-to-r from-blue-500 to-[#E114E5] bg-clip-text text-transparent">
+                the largest figma UI kit
+              </span>
+            </h2>
+            <p className="mx-auto max-w-2xl">
+              Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+              doloremque laudantium, totam rem aperiam, eaque ipsa quae.
+            </p>
+            <div className="items-center justify-center gap-x-3 space-y-3 text-sm sm:flex sm:space-y-0">
+              <a
+                href="/"
+                className="block rounded-lg bg-blue-600 px-4 py-2 font-medium text-white shadow-lg duration-150 hover:bg-blue-500 hover:shadow-none active:bg-blue-700"
+              >
+                Browse courses
+              </a>
+
+              <a
+                href="/"
+                className="block rounded-lg border border-slate-700 px-4 py-2 font-medium text-slate-200 duration-150 hover:bg-slate-800"
+              >
+                Get access
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
